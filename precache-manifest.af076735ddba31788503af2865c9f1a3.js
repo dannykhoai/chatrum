5d3e753f1c0a63cce1b81c7986e086ac50376694
dannykhoai <dvu2758@gmail.com>
2020-11-16 09:53:08 +0200

    Updates

---
+++ precache-manifest.af076735ddba31788503af2865c9f1a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f45e091a370ec403b2beaf8efeead728",
+    "url": "/chatrum/index.html"
+  },
+  {
+    "revision": "514b771cb89bcbd9ef8d",
+    "url": "/chatrum/static/css/main.ebb9c479.chunk.css"
+  },
+  {
+    "revision": "b79ee1dcca7d23ff2143",
+    "url": "/chatrum/static/js/2.59255022.chunk.js"
+  },
+  {
+    "revision": "4712edae9788d237216439aee9b2a386",
+    "url": "/chatrum/static/js/2.59255022.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "514b771cb89bcbd9ef8d",
+    "url": "/chatrum/static/js/main.9510cb7f.chunk.js"
+  },
+  {
+    "revision": "8442cdd1e1a26721d20e",
+    "url": "/chatrum/static/js/runtime-main.fe9cd071.js"
+  }
+]);
